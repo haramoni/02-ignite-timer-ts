8081ed7ce1189eeb13c66784e91256e5c7607a20
Júlia Haramoni <haramoni.julia@gmail.com>
2025-04-25 11:01:04 -0300

    iniciando o uso de reducers

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { CyclesContext } from "../../contexts/CyclesContext";
 import { HistoryContainer, HistoryList, Status } from "./styles";
 import { formatDistanceToNow } from "date-fns";
-import { pt, ptBR } from "date-fns/locale";
+import { ptBR } from "date-fns/locale";
 
 export function History() {
   const { cycles } = useContext(CyclesContext);
